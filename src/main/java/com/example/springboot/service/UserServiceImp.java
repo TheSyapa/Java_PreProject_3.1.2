7e724619a0613e770ee9f5f510d35388a1a46c30
Syapa <sergeyofit@mail.ru>
2023-02-03 23:12:56 +0300

    1

---
+++ src/main/java/com/example/springboot/service/UserServiceImp.java
@@ -0,0 +1,36 @@
+package com.example.springboot.service;
+
+import com.example.springboot.dao.UserDao;
+import com.example.springboot.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImp implements UserService {
+
+    private final UserDao userDao;
+
+    @Autowired
+    public UserServiceImp(UserDao userDao) {
+        this.userDao = userDao;
+    }
+    public List<User> findAll(){
+        return userDao.findAll();
+    }
+
+    public User saveUser(User user){
+        return userDao.save(user);
+    }
+
+    public void deleteById(Long id){
+        userDao.deleteById(id);
+    }
+
+    public User findById(Long id){
+
+        return  userDao.getOne(id);
+    }
+
+}
