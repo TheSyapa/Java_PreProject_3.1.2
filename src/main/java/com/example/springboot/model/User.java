8a67ee0ddb535eba836a6ccb8482a2bc1ac3236e
Syapa <sergeyofit@mail.ru>
2023-02-03 23:14:42 +0300

    1

--- src/main/java/com/example/springboot/model/User.java
+++ src/main/java/com/example/springboot/model/User.java
@@ -1,16 +1,13 @@
 package com.example.springboot.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
 
-@Data
 @Entity
 @Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private Long id;
 
     @Column(name = "first_name")
@@ -19,8 +16,38 @@ public class User {
     @Column(name = "last_name")
     private String lastName;
 
-    @Column(name = "salary")
     private long salary;
 
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public long getSalary() {
+        return salary;
+    }
+
+    public void setSalary(long salary) {
+        this.salary = salary;
+    }
 }
