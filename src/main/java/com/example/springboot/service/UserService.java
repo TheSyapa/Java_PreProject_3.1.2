4162737281e7f3e980f9f321a21e8dfdc9c8308a
Syapa <sergeyofit@mail.ru>
2023-02-03 23:12:46 +0300

    1

--- src/main/java/com/example/springboot/service/UserService.java
+++ src/main/java/com/example/springboot/service/UserService.java
@@ -1,35 +1,15 @@
 package com.example.springboot.service;
 
-import com.example.springboot.dao.UserDao;
 import com.example.springboot.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public class UserService {
+public interface UserService {
+    public List<User> findAll();
 
-    private final UserDao userDao;
+    public User saveUser(User user);
 
-    @Autowired
-    public UserService(UserDao userDao) {
-        this.userDao = userDao;
-    }
-    public List<User> findAll(){
-        return userDao.findAll();
-    }
+    public void deleteById(Long id);
 
-    public User saveUser(User user){
-        return userDao.save(user);
-    }
-
-    public void deleteById(Long id){
-        userDao.deleteById(id);
-    }
-
-    public User findById(Long id){
-        return  userDao.getOne(id);
-    }
-
+    public User findById(Long id);
 }
