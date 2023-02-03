987e49426e4ced6ede2727553c5a889e1622e335
Syapa <sergeyofit@mail.ru>
2023-02-03 16:42:17 +0300

    Initial commit

---
+++ src/main/java/com/example/springboot/model/User.java
@@ -0,0 +1,26 @@
+package com.example.springboot.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "salary")
+    private long salary;
+
+
+}
